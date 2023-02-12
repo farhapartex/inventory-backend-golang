6a079b4721b9ff4b11d76359416b1f4c8a8a97c8
Md Nazmul Hasan <hasan08sust@gmail.com>
2023-02-13 02:22:57 +0600

    Added Register controller

---
+++ controller/authentication.go
@@ -0,0 +1,31 @@
+package controller
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/goupp-backend/model"
+	"net/http"
+)
+
+func Register(context *gin.Context){
+	var input model.AuthenticationInput
+	if err := context.ShouldBindJSON(&input); err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	user := model.User{
+		FirstName: input.FirstName,
+		LastName: input.LastName,
+		Username: input.Username,
+		Password: input.Password,
+	}
+
+	savedUser, err := user.Save()
+
+	if err != nill{
+		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	context.JSON(http.StatusCreated, gin.H{"user": savedUser})
+}
--- model/authenticationInput.go
+++ model/authenticationInput.go
@@ -1,6 +1,8 @@
 package model
 
 type AuthenticationInput struct {
+	FirstName string `json:"first_name"`
+	LastName string `json:"last_name"`
     Username string `json:"username" binding:"required"`
     Password string `json:"password" binding:"required"`
 }
