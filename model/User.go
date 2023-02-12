565a05c193c6a8899128d7b81f99e710c2e2e596
Md Nazmul Hasan <hasan08sust@gmail.com>
2023-02-13 00:36:47 +0600

    Updated User model

--- model/User.go
+++ model/User.go
@@ -4,10 +4,10 @@ import "gorm.io/gorm"
 
 type User struct {
 	gorm.Model
-	FirstName string `json:"first_name"`
-	LastName  string `json:"last_name"`
-	Username string `json:"username"`
-	Password string `json:"password"`
+	FirstName string `gorm:"size:255;not null;" json:"first_name"`
+	LastName  string `gorm:"size:255:not null;" json:"last_name"`
+	Username string `gorm:"size:255;not null;unique" json:"username"`
+	Password string `gorm:"size:255;not null" json:"-"`
 	IsSuperAdmin bool `json:"is_super_admin"`
 	IsCustomer bool `json:"is_customer"`
 }
